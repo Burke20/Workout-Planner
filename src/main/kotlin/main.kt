a87d8c381dc52656fe3bcd8f08c1d2db31e6923f
burke20 <cuanburke10@hotmail.com>
2023-12-01 18:59:44 +0000

    Created a basic main menu for my Workout App

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,58 @@
+import java.lang.System.exit
+import java.util.*
+
+val scanner = Scanner(System.`in`)
+fun main(args: Array<String>) {
+    runMenu()
+}
+fun runMenu() {
+
+    fun mainMenu() : Int {
+        print(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Create a Workout Plan     |
+         > |   2) List all Workout Plans    |
+         > |   3) Update a Workout Plan     |
+         > |   4) Delete a Workout Plan     |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+        return scanner.nextInt()
+    }
+
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> createWorkoutPlan()
+            2  -> listWorkoutPlans()
+            3  -> updateWorkoutPlans()
+            4  -> deleteWorkoutPlans()
+            0  -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
+}
+fun createWorkoutPlan(){
+    println("You chose Add Note")
+}
+
+fun listWorkoutPlans(){
+    println("You chose List Notes")
+}
+
+fun updateWorkoutPlans(){
+    println("You chose Update Note")
+}
+
+fun deleteWorkoutPlans(){
+    println("You chose Delete Note")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
+}
--- src/main/main.kt
+++
@@ -1,6 +0,0 @@
-private val noteAPI = NoteAPI(YAMLSerialiser(File("notes.YAML")))
-private val logger = KotlinLogging.logger {}
-
-fun main(args: Array<String>) {
-    runMenu()
-}

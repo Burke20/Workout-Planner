8b5f2b9a2da463bbc47aa3ad1354850ca25d3483
burke20 <cuanburke10@hotmail.com>
2023-12-01 21:21:21 +0000

    Added to the controller class WorkoutPlannerAPI. Added 2 new functions create() and listAllWorkoutPlans()

--- src/main/kotlin/controllers/WorkoutPlannerAPI.kt
+++ src/main/kotlin/controllers/WorkoutPlannerAPI.kt
@@ -1,4 +1,25 @@
 package controllers
 
+import models.WorkoutPlan
+
 class WorkoutPlannerAPI {
+    private var workoutPlans = ArrayList<WorkoutPlan>()
+
+
+    fun create(workoutPlan: WorkoutPlan): Boolean {
+        return workoutPlans.add(workoutPlan)
+    }
+
+    fun listAllWorkoutPlans(): String {
+        return if (workoutPlans.isEmpty()) {
+            "No workout Plans Stored"
+        } else {
+            var listOfWorkoutPlans = ""
+            for (i in workoutPlans.indices) {
+                listOfWorkoutPlans += "${i}: ${workoutPlans[i]} \n"
+            }
+            listOfWorkoutPlans
+        }
+    }
+
 }
